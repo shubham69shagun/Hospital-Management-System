5081ddc69366bad049402eb3c8179254d5709472
Shubham Kumar <shubhamk.ug19.cs@nitp.ac.in>
2021-07-11 15:52:37 +0530

    Failure

---
+++ include/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
+	<script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/fontawesome.min.css">
+	<link rel="stylesheet" href="header.css" type = "text/css">
+	<style>	
+	</style>
+
+</head>
+<body>
+
+	<nav class="mynav navbar navbar-expand-lg navbar-light bg-primary " >
+	  <div class="container-fluid " id="abc">
+	    <a class="navbar-brand " href="#">Hospital Management System</a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarNav">
+	      <ul class="navbar-nav">
+	        <li class="nav-item">
+	          <a class="nav-link active" aria-current="page" href="#">Admin</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#">Patient</a>
+	        </li>
+	        
+	      </ul>
+	    </div>
+	  </div>
+</nav>
+	
+</body>
+</html>
